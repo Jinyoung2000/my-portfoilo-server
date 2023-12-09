21fd015204392bea094568709ae1a4c427c0bfbc
Jinyoung Sung <sjy021621@gmail.com>
2023-12-09 23:39:03 +0900

    initial commit

---
+++ src/main/kotlin/com/jinyoung2000/myportfolioserver/MyPortfolioServerApplication.kt
@@ -0,0 +1,12 @@
+package com.jinyoung2000.myportfolioserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyPortfolioServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<MyPortfolioServerApplication>(*args)
+}
+
---
+++ src/test/kotlin/com/jinyoung2000/myportfolioserver/MyPortfolioServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jinyoung2000.myportfolioserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyPortfolioServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
